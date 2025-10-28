6656ffd8fb071e2a517b4573fb68f5c216f1f53a
StockOslo <erik.antonov@icloud.com>
2025-10-28 20:46:32 +0300

    Initial Commit

---
+++ Electronics store/Electronics_storeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Electronics_storeApp.swift
+//  Electronics store
+//
+//  Created by Erik Antonov on 28.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct Electronics_storeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Electronics storeTests/Electronics_storeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Electronics_storeTests.swift
+//  Electronics storeTests
+//
+//  Created by Erik Antonov on 28.10.2025.
+//
+
+import Testing
+@testable import Electronics_store
+
+struct Electronics_storeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
