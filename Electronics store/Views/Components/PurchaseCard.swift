566408c71c95de9e2c090dc8bfeb007f91d25bef
StockOslo <erik.antonov@icloud.com>
2025-11-08 16:11:48 +0300

    Добавлена локализация, проведены некоторые изменения в  разметки проекта

---
+++ Electronics store/Views/Components/PurchaseCard.swift
@@ -0,0 +1,18 @@
+//
+//  PurchaseCard.swift
+//  Electronics store
+//
+//  Created by Erik Antonov on 05.11.2025.
+//
+
+import SwiftUI
+
+struct PurchaseCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PurchaseCard()
+}
